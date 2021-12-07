7f22e121190ed4a30ae80dca88f4b9ecb0803766
Nina Popovic <majsalica@icloud.com>
2021-12-07 18:48:06 +0100

    Add placeholder property in Input component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,5 +1,5 @@
-export default function Input({type, name, id, className, value}) {
+export default function Input({type, name, id, className, value, placeholder}) {
     return (
-        <input type={type} value={value} className={className} name={name} id={id} />
+        <input type={type} value={value} className={className} name={name} id={id} placeholder={placeholder} />
     )
 }
